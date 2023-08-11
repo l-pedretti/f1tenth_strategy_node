2efb7e62cfe6567553cf67b67150634c0c97226d
l-pedretti <pedroleon99@hotmail.it>
2023-08-11 16:23:58 +0200

    Removed useless folders from repository

--- build/f1tenth_strategy_node/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/f1tenth/Desktop/project/install/f1tenth_strategy_node'
--- build/f1tenth_strategy_node/share/f1tenth_strategy_node/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/f1tenth/Desktop/project/build/f1tenth_strategy_node"
